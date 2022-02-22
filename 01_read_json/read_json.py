52fd4daf5f44bc6c15df80569ff2a3d0dc5e9618
arielattias1 <74975408+arielattias1@users.noreply.github.com>
2022-02-22 10:36:16 -0500

    Add files via upload

--- 01_read_json/read_json.py
+++ 01_read_json/read_json.py
@@ -1,6 +1,7 @@
 # ---------------------------------------------
 # import package to read json
 # ---------------------------------------------
+import json
 
 
 # ---------------------------------------------
@@ -8,18 +9,22 @@
 # ---------------------------------------------
 
 def readJson(file):
-    pass
+    with open(file) as p:
+        return json.load(p)
 
 # ---------------------------------------------
 #  call 'readJson', load salaries
 # ---------------------------------------------
 
+salaries = readJson('/Users/arielattias/Desktop/Dropbox/MIT/Classes/1.001/read-parse-validate/01_read_json/data.json')
+print(salaries)
 
 # ---------------------------------------------
 #  print all salaries
 # ---------------------------------------------
 
-
+for salary in salaries['data']:
+    print(salary[18])
 # ---------------------------------------------
 # loop through list, only print salary field
 # ---------------------------------------------
