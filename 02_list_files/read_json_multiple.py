daee7b6c91b0c165bf83530a5deda058cd215040
arielattias1 <74975408+arielattias1@users.noreply.github.com>
2022-02-22 10:37:33 -0500

    Add files via upload

--- 02_list_files/read_json_multiple.py
+++ 02_list_files/read_json_multiple.py
@@ -2,13 +2,29 @@
 # import package to read json
 # import package to walk file system
 # -------------------------------------
-
+from asyncore import read
+import json 
+import glob
 
 # -----------------------------------
 #  list all files
 # -----------------------------------
 
+data = []
 
+pattern = '/Users/arielattias/Desktop/Dropbox/MIT/Classes/1.001/read-parse-validate/01_read_json/data/*/*.json'
+for file in glob.glob(pattern):
+    data.append(file)
+
+print(data)
 # -----------------------------------
 #  loop through files, parse json
-# -----------------------------------
+# ---------------------------------
+
+def readJson(file):
+    with open(file) as p: 
+        return json.load(p)
+
+for file in data: 
+    banana = readJson(file)
+    print(banana)
